747e062458e2e31f137784c666c97992840c9fd1
INTERNE\bwarembourg <boris.warembourg@ext.imprimerienationale.fr>
2017-03-27 11:39:29 +0200

    bugfix image loader

--- js/fmk/assets/graphics/renderer.js
+++ js/fmk/assets/graphics/renderer.js
@@ -6,36 +6,38 @@ function Renderer() {
 };
 
 Renderer.prototype.render = function() {
-    CTX.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    if (ASSET_MANAGER.assetLoader.isReady) {
+        CTX.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-    for (var i = 0; i < this.renderables.length; i++) {
-        var renderable = this.renderables[i];
-        var crop = renderable.crop;
-        var spritesheet = ASSET_MANAGER.getSpritesheet(crop.spritesheetName);
+        for (var i = 0; i < this.renderables.length; i++) {
+            var renderable = this.renderables[i];
+            var crop = renderable.crop;
+            var spritesheet = ASSET_MANAGER.getSpritesheet(crop.spritesheetName);
 
-        CTX.drawImage(
-            spritesheet, 
-            crop.sourceX, crop.sourceY, 
-            crop.sourceWidth, crop.sourceHeight, 
-            renderable.canvasX, renderable.canvasY, 
-            crop.sourceWidth, crop.sourceHeight
-        );
+            CTX.drawImage(
+                spritesheet, 
+                crop.sourceX, crop.sourceY, 
+                crop.sourceWidth, crop.sourceHeight, 
+                renderable.canvasX, renderable.canvasY, 
+                crop.sourceWidth, crop.sourceHeight
+            );
 
-        for(var j = 0; j < this.texts.length; j++){
+            for(var j = 0; j < this.texts.length; j++){
 
-            if(this.texts[j]!=null && this.texts[j].toDisplay && STATE_MANAGER.currentState.stateName=="Game"){
-                CTX.font = this.texts[j].font;
-                CTX.fillStyle = this.texts[j].color;
-                this.texts[j].getText(CTX);
+                if(this.texts[j]!=null && this.texts[j].toDisplay && STATE_MANAGER.currentState.stateName=="Game"){
+                    CTX.font = this.texts[j].font;
+                    CTX.fillStyle = this.texts[j].color;
+                    this.texts[j].getText(CTX);
+                }
+            }
+            if(this.life.toDisplay){
+                CTX.fillStyle = this.life.fillStyle;
+                CTX.fillRect(LIFE_X, LIFE_Y, this.life.width, this.life.height);
             }
         }
-        if(this.life.toDisplay){
-            CTX.fillStyle = this.life.fillStyle;
-            CTX.fillRect(LIFE_X, LIFE_Y, this.life.width, this.life.height);
-        }
+        this.renderables = null;
+        this.renderables = [];
     }
-    this.renderables = null;
-    this.renderables = [];
 };
 
 Renderer.prototype.addToRenderList = function(crop, destX, destY) {
